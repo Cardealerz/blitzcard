1d9add38455505ae852eb4c5ea858e4d594b3142
Carlos Puello <carlitosthb@hotmail.com>
2021-03-26 06:13:55 -0500

    Search (#14)
    * Login system and user model (#5)
    
    * [UD] Register user view (#6)
    
    * Codes (#7)
    
    * [ADD] Codes, cart
    
    * [UD] Code shopping
    
    * [UD] Codes
    
    * Feature/b4 payment history (#9)
    
    * Create model and migration
    
    * Modify controller
    
    * Correction in controller
    
    * Shopping car fix
    
    * Feature/b4 payment history (#10)
    
    * Create model and migration
    
    * Modify controller
    
    * Correction in controller
    
    * Shopping car fix
    
    * Creation of views
    
    * Create PDF
    
    * [ADD] Mailer (#11)
    
    * [ADD] Mailer
    
    * [ADD] Bootstrap style
    
    * [ADD] search
    
    * [FIX] Show one view
    
    Co-authored-by: Ricardo Gottheil <ricardogottheil@gmail.com>
    Co-authored-by: Andrés López <andresjlopez12@gmail.com>

--- resources/views/payHistory/showOne.blade.php
+++ resources/views/payHistory/showOne.blade.php
@@ -24,33 +24,30 @@
                     <b>{{__('labels.status')}}:</b> {{ $payHistory->getPaymentStatus() }}<br />
                     <b>{{__('labels.date')}}:</b> {{ $payHistory->getPaymentDate() }}<br />
                     @if ($payHistory->getPaymentType() == "order")
-                        <b>{{__('labels.codes')}}: </b><br />
+                    <b>{{__('labels.codes')}}: </b><br />
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">{{__('labels.name')}}</th>
+                                <th scope="col">{{__('labels.code')}}</th>
+                            </tr>
+                        </thead>
 
-                        <table class="table table-bordered">
-                            <thead>
-                                <tr>  
-                                    <th scope="col">{{__('labels.name')}}</th>
-                                    <th scope="col">{{__('labels.code')}}</th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-                                @forelse($payHistory->codes() as $code)
-                                <tr>
-                                    <td>{{$code["name"]}}</td> 
-                                    <td>{{$code["code"]}}</td>
-                                </tr>
-                                @empty
-                                <b>{{__('messages.no_codes')}}</b><br />
-                                @endforelse
-                            </tbody>
-                        </table>
+                        <tbody>
+                            @forelse($payHistory->codes() as $code)
+                            <tr>
+                                <td>{{$code["name"]}}</td>
+                                <td>{{$code["code"]}}</td>
+                            </tr>
+                            @empty
+                            <b>{{__('messages.no_codes')}}</b><br />
+                            @endforelse
+                        </tbody>
+                    </table>
                     @else
-                        <b>{{__('labels.payment_method')}}:</b> {{ $payHistory->getPaymentMethod() }}<br />
+                    <b>{{__('labels.payment_method')}}:</b> {{ $payHistory->getPaymentMethod() }}<br />
                     @endif
-                    
-                    <a class="btn btn-primary" href="{{ route('codeTemplate.list') }}">{{__('labels.back')}}</a><br /><br />
-                   
+                    <a class="btn btn-primary" href="{{ route('payhistory.showAll') }}">{{__('labels.back')}}</a><br /><br />
                 </div>
             </div>
         </div>
